68727b2b1c7729edf2d2735e71c18f73467cc1e5
perezkarinan <107373605+perezkarinan@users.noreply.github.com>
2022-08-27 19:48:38 -0400

    made comments and added a 0 for hours and minutes

--- javascript/weather.js
+++ javascript/weather.js
@@ -2,9 +2,15 @@ function currentDate() {
   return new Date();
 }
 let now = new Date();
-
+//getting current date and time // hours and minutes
 let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
 let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 let days = [
   "Sunday",
   "Monday",
@@ -15,10 +21,10 @@ let days = [
   "Saturday",
 ];
 let day = days[now.getDay()];
-
+// adding date into HTML
 let h2 = document.querySelector("#date");
 h2.innerHTML = `${day}`;
-
+//adding time to html
 let h3 = document.querySelector("#time");
 h3.innerHTML = `${hour}:${minutes}`;
 //current temp
@@ -28,7 +34,7 @@ function currentTemp(response) {
   let h4 = document.querySelector("#temperature");
   h4.innerHTML = showTemp;
 }
-
+//adding city searched into h1
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-input");
@@ -37,25 +43,28 @@ function search(event) {
   //changing temp
   let city = searchInput.value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4bc3938ce8d1f5e4234ae8d08954dd0d&units=imperial`;
+
   axios.get(apiUrl).then(currentTemp);
 }
+
+//clicking submit button
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
-
+//converting fahrenheit
 function convertFahrenheit(event) {
   event.preventDefault();
   let temp = document.querySelector("#temperature");
   temp.innerHTML = 63;
 }
-
+//converting celsius
 function convertCelsius(event) {
   event.preventDefault();
   let temp = document.querySelector("#temperature");
   temp.innerHTML = 17;
 }
-
+//clicking fahrenheit
 let fahrenheit = document.querySelector("#fahrenheit");
 fahrenheit.addEventListener("click", convertFahrenheit);
-
+//clicking celsius
 let celsius = document.querySelector("#celsius");
 celsius.addEventListener("click", convertCelsius);
