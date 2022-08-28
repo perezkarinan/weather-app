0c9b0736d72f96805b0c6ac63f67744633b98f7b
perezkarinan <107373605+perezkarinan@users.noreply.github.com>
2022-08-27 21:58:48 -0400

    fixed functions and id's in JS&html

--- javascript/weather.js
+++ javascript/weather.js
@@ -30,41 +30,35 @@ h3.innerHTML = `${hour}:${minutes}`;
 //current temp
 
 function currentTemp(response) {
-  let showTemp = Math.round(response.data.main.temp);
-  let h4 = document.querySelector("#temperature");
-  h4.innerHTML = showTemp;
-}
-//adding city searched into h1
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#city-input");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${searchInput.value}`;
-  //changing temp
-  let city = searchInput.value;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4bc3938ce8d1f5e4234ae8d08954dd0d&units=imperial`;
+  let tempElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity-input");
+  let wind = document.querySelector("#wind-input");
 
+  fahrenheitTemp = response.data.main.temp;
+
+  tempElement.innerHTML = Math.round(fahrenheitTemp);
+  cityElement.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
+
+//adding city searched into h1
+function search(city) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4bc3938ce8d1f5e4234ae8d08954dd0d&units=imperial`;
   axios.get(apiUrl).then(currentTemp);
+}
+
+function handleSearchButton(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  search(cityInput.value);
 }
 
 //clicking submit button
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
-//converting fahrenheit
-function convertFahrenheit(event) {
-  event.preventDefault();
-  let temp = document.querySelector("#temperature");
-  temp.innerHTML = 63;
-}
-//converting celsius
-function convertCelsius(event) {
-  event.preventDefault();
-  let temp = document.querySelector("#temperature");
-  temp.innerHTML = 17;
-}
-//clicking fahrenheit
-let fahrenheit = document.querySelector("#fahrenheit");
-fahrenheit.addEventListener("click", convertFahrenheit);
-//clicking celsius
-let celsius = document.querySelector("#celsius");
-celsius.addEventListener("click", convertCelsius);
+form.addEventListener("submit", handleSearchButton);
+
+search("New York");
